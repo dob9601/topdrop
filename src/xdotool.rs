3076c9fef4781aef0f37fe78489312a77f489310
Daniel O'Brien <dob9601@gmail.com>
2023-07-28 16:52:15 +0100

    Improve stability

--- src/xdotool.rs
+++ src/xdotool.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, process::Command};
+use std::{error::Error, process::Command, thread::sleep, time::Duration};
 
 use clap::ValueEnum;
 use strum::Display;
@@ -24,7 +24,7 @@ pub struct Window(pub u32);
 
 impl Window {
     pub fn find_by_pid(pid: u32) -> Result<Window, Box<dyn Error>> {
-        let window_id = run_xdotool(&["search", "--pid", pid.to_string().as_str()])?;
+        let window_id = run_xdotool(&["search", "--pid", pid.to_string().as_str()], false)?;
 
         Ok(Window(window_id.trim().parse()?))
     }
@@ -33,30 +33,36 @@ impl Window {
         let width = x2 - x1;
         let height = y2 - y1;
 
-        run_xdotool(&[
-            "windowsize",
-            self.0.to_string().as_str(),
-            width.to_string().as_str(),
-            height.to_string().as_str(),
-        ])?;
+        run_xdotool(
+            &[
+                "windowsize",
+                self.0.to_string().as_str(),
+                width.to_string().as_str(),
+                height.to_string().as_str(),
+            ],
+            true,
+        )?;
 
-        run_xdotool(&[
-            "windowmove",
-            self.0.to_string().as_str(),
-            x1.to_string().as_str(),
-            y1.to_string().as_str(),
-        ])?;
+        run_xdotool(
+            &[
+                "windowmove",
+                self.0.to_string().as_str(),
+                x1.to_string().as_str(),
+                y1.to_string().as_str(),
+            ],
+            true,
+        )?;
 
         Ok(())
     }
 
     pub fn hide(&self) -> Result<(), Box<dyn Error>> {
-        run_xdotool(&["windowunmap", self.0.to_string().as_str()])?;
+        run_xdotool(&["windowunmap", self.0.to_string().as_str()], true)?;
         Ok(())
     }
 
     pub fn unhide(&self) -> Result<(), Box<dyn Error>> {
-        run_xdotool(&["windowmap", self.0.to_string().as_str()])?;
+        run_xdotool(&["windowmap", self.0.to_string().as_str()], true)?;
         Ok(())
     }
 
@@ -69,34 +75,63 @@ impl Window {
     }
 
     pub fn add_window_state(&self, window_state: WindowState) -> Result<(), Box<dyn Error>> {
-        run_xdotool(&[
-            "windowstate",
-            "--add",
-            window_state.to_string().as_str(),
-            self.0.to_string().as_str(),
-        ])?;
+        run_xdotool(
+            &[
+                "windowstate",
+                "--add",
+                window_state.to_string().as_str(),
+                self.0.to_string().as_str(),
+            ],
+            true,
+        )?;
         Ok(())
     }
 
     pub fn remove_window_state(&self, window_state: WindowState) -> Result<(), Box<dyn Error>> {
-        run_xdotool(&[
-            "windowstate",
-            "--remove",
-            window_state.to_string().as_str(),
-            self.0.to_string().as_str(),
-        ])?;
+        run_xdotool(
+            &[
+                "windowstate",
+                "--remove",
+                window_state.to_string().as_str(),
+                self.0.to_string().as_str(),
+            ],
+            true,
+        )?;
         Ok(())
     }
 }
 
-pub fn run_xdotool(args: &[&str]) -> Result<String, Box<dyn Error>> {
+fn _run_xdotool(args: &[&str]) -> Result<String, Box<dyn Error>> {
     let output = Command::new("xdotool").args(args).output()?;
 
     if let Some(code) = output.status.code() {
+        let stderr = std::str::from_utf8(&output.stderr)?;
+        let stdout = std::str::from_utf8(&output.stderr)?;
         if code != 0 {
-            return Err("xdotool failed to run".into());
+            return Err(
+                format!("xdotool failed to run:\nSTDOUT:\n{stdout}\n\nSTDERR:\n{stderr}").into(),
+            );
         }
     }
 
     Ok(std::str::from_utf8(&output.stdout)?.to_string())
+}
+
+/// Attempt to run Xdotool with up to 5 retries
+pub fn run_xdotool(args: &[&str], retry: bool) -> Result<String, Box<dyn Error>> {
+    let mut counter = 0;
+
+    loop {
+        match _run_xdotool(args) {
+            Ok(output) => return Ok(output),
+            Err(err) => {
+                if counter >= 5 || !retry {
+                    return Err(err);
+                }
+                counter += 1;
+                dbg!("Retrying");
+                sleep(Duration::from_millis(50))
+            }
+        }
+    }
 }
